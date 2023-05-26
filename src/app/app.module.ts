749a6fabc5fa9020622e1f9c840cece3f735ab97
alex keremy <keremy.alex@gmail.com>
2023-05-26 19:29:37 -0400

    feat: contador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    HeroComponent,
   ],
   imports: [
     BrowserModule
