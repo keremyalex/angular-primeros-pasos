1c92840f9756c047733f7bbb3b7de497ff852213
alex keremy <keremy.alex@gmail.com>
2023-05-28 22:06:03 -0400

    primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/characters.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ];
+
+
+  // onDelete = Index value: number
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string): void{
+
+    if(!id) return;
+
+    this.onDelete.emit(id);
+  }
+
+}
